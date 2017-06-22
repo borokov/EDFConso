105202a94c93a04aa0f3060cfd31a086d116fcf0
pyros <pyrosfr@gmail.com>
2017-06-22 21:41:03 +0200

    Fix when value are corrupted: return error message

--- doAdd.php
+++ doAdd.php
@@ -23,9 +23,13 @@ if ( ($lastHC <= $heure_creuse && $heure_creuse < $lastHC + 100000)
   $sql = 'INSERT INTO conso (hc, hp, date) VALUES ('.$heure_creuse.','.$heure_pleine.', now())';
 
   mysql_query ($sql) or die ('Erreur SQL : '.$sql.'<br />'.mysql_error()); 
+  echo("done");
+}
+else
+{
+  echo("Corrupted value");
 }
 // on ferme la connexion
 mysql_close();
 
-echo("done");
 ?>
