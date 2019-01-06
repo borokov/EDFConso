80d6716dcc21d4fe3ea13361e96008b71bf7688b
pyros <pyrosfr@gmail.com>
2019-01-06 09:44:20 +0100

    Add some check to log only value at the begining of hour

--- doAdd.php
+++ doAdd.php
@@ -1,33 +1,40 @@
 <?php
 include("connectSql.php");
 $base = connectMaBase();
-// ex: http://myServer/EDF/doAdd.php?hc=55801068&hp=124984298
+// ex: http://myServer/EDF/doAdd.php?hc=55801068&hp=124984298&I=5
 
-//On recupere les valeurs entrees par l'utilisateur :
-$heure_creuse = $_POST['hc'];
-$heure_pleine = $_POST['hp'];
+// Get inputs
+// Add offset corresponding to last report before changing to linky (because linky reset to 0)
+$heure_creuse = $_POST['hc'] + 63830532;
+$heure_pleine = $_POST['hp'] + 133789932;
+$IInst = $POST['I'];
 
-// seelct last entry to perform sanity check
+$current_hour = date("H");
+
+// select last entry to perform sanity check
 $sql = 'SELECT * FROM conso ORDER BY date DESC LIMIT 1';
 $req = mysqli_query($base, $sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysqli_error($base));
 $data = mysqli_fetch_array($req);
 $lastHC = $data['hc'];
 $lastHP = $data['hp'];
+$lastHour = date("H", strtotime($data['date']));
 
 // sanity check because sometime values are currupted. Ensure monitored HC/HP is > last entry
 // and < last entry + some amount
 if ( ($lastHC <= $heure_creuse && $heure_creuse < $lastHC + 100000)
    && ($lastHP <= $heure_pleine && $heure_pleine < $lastHP + 100000) )
 {
-  //On prepare la commande sql d'insertion
-  $sql = 'INSERT INTO conso (hc, hp, date) VALUES ('.$heure_creuse.','.$heure_pleine.', now())';
-
-  mysqli_query($base, $sql) or die ('Erreur SQL : '.$sql.'<br />'.mysqli_error($base)); 
+  // If measure correspond to a new hour, insert value in database
+  if ( $lastHour != $current_hour )
+  {
+    $sql = 'INSERT INTO conso (hc, hp, date) VALUES ('.$heure_creuse.','.$heure_pleine.', now())';
+    mysqli_query($base, $sql) or die ('Erreur SQL : '.$sql.'<br />'.mysqli_error($base)); 
+  }
   echo("done");
 }
 else
 {
-//  echo("Corrupted value");
+  //echo("Corrupted value");
   echo("done");
 }
 // on ferme la connexion
